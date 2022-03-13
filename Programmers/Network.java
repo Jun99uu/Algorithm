ec940a253c2d35b37edacc89f58a72023006a625
Jun99uu <igun0423@naver.com>
2022-03-13 13:09:07 +0900

    DFS

---
+++ Programmers/Network.java
@@ -0,0 +1,26 @@
+class Solution {
+    boolean [] check;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        check = new boolean [n];
+        
+        for(int i = 0; i < n; i++){
+            if(!check[i]){
+                dfs(computers, i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    public void dfs(int [][] computers, int i){
+        check[i] = true;
+        for(int j = 0; j < computers.length; j++){
+            if(i != j && computers[i][j] == 1 && check[j] == false){
+                dfs(computers, j);
+            }
+        }
+    }
+}
